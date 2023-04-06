ee44d1190e43151b313903f8c1aab22b325d48e1
aimenhamed <aimenhamed@live.com.au>
2023-04-06 17:31:08 +1000

    fix endpoint

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,6 +1,8 @@
 export default async function fetcher(url: string) {
   const endpoint =
-    process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "http://predle.vercel.app";
 
   const res = await fetch(`${endpoint}${url}`, { cache: "no-store" });
   return res.json();
