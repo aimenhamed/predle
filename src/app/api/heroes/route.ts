d9520ce3030ef2300dc7e461226239c2997cb50d
aimenhamed <aimenhamed@live.com.au>
2023-04-06 16:55:28 +1000

    initial commit

---
+++ src/app/api/heroes/route.ts
@@ -0,0 +1,43 @@
+type Position = "Tank" | "Assassin" | "Mage" | "Support" | "Marksman";
+
+type Hero = {
+  name: string;
+  gender: "Male" | "Female" | "Other";
+  position: Position[];
+};
+
+export async function GET(request: Request) {
+  const heroes: Hero[] = [
+    { name: "Countess", gender: "Female" },
+    { name: "Crunch", gender: "Other" },
+    { name: "Dekker", gender: "Female" },
+    { name: "Drongo", gender: "Male" },
+    { name: "Feng Mao", gender: "Male" },
+    { name: "Gadget", gender: "Female" },
+    { name: "Gideon", gender: "Male" },
+    { name: "Grux", gender: "Male" },
+    { name: "Howitzer", gender: "Male" },
+    { name: "Kallari", gender: "Female" },
+    { name: "Khaimera", gender: "Male" },
+    { name: "Muriel", gender: "Female" },
+    { name: "Murdock", gender: "Male" },
+    { name: "Narbash", gender: "Male" },
+    { name: "Riktor", gender: "Male" },
+    { name: "Sevarog", gender: "Male" },
+    { name: "Steel", gender: "Male" },
+    { name: "Shinbi", gender: "Female" },
+    { name: "Sparrow", gender: "Female" },
+    { name: "The Fey", gender: "Female" },
+    { name: "Revenant", gender: "Male" },
+    { name: "Rampage", gender: "Male" },
+    { name: "Kira", gender: "Female" },
+    { name: "Lt. Belica", gender: "Female" },
+  ];
+
+  const res = {
+    heroes,
+    count: heroes.length,
+  };
+
+  return new Response(JSON.stringify(res));
+}
---
+++ src/app/components/HeroInput.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useState } from "react";
+
+type HeroInputProps = {
+  todaysHero: string;
+  heroes: any[];
+};
+
+export default function HeroInput({ todaysHero, heroes }: HeroInputProps) {
+  const [value, setValue] = useState<string>("");
+  const [guesses, setGuesses] = useState<string[]>([]);
+  const [correct, setCorrect] = useState<boolean>(false);
+
+  const guess = async () => {
+    if (value === todaysHero) {
+      setCorrect(true);
+      alert("Correct!");
+    } else {
+      setValue("");
+      setGuesses([...guesses, value]);
+    }
+  };
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div>
+        <select onChange={(e) => setValue(e.target.value)}>
+          {heroes.map((h, idx) => (
+            <option id={h.name + idx} value={h.name}>
+              {h.name}
+            </option>
+          ))}
+        </select>
+        <button onClick={guess}>Enter</button>
+      </div>
+      {correct && <>Correct! Today's hero is: {todaysHero}</>}
+      <h3>Previous guesses {guesses.length}:</h3>
+      {guesses.length > 0 && (
+        <div>
+          <ul>
+            {guesses.map((guess) => (
+              <li id={guess}>{guess}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
